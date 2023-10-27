14396b9f39e7f01f24cbe2c4439cd5b8a88d338e
aniketbhatia28 <aniketbhatia28@gmail.com>
2023-10-27 19:14:24 +0530

    Updated blog UI

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,12 @@ import Date from '../../components/date';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import utilStyles from '../../styles/utils.module.css';
+import Container from '../../components/container';
+import Header from '../../components/header';
+import PostHeader from '../../components/post-header';
+import PostBody from '../../components/post-body';
+import styles from '../../components/layout.module.css';
+import Link from 'next/link';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -24,16 +30,27 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
     return (
       <Layout>
-        <Head>
-          <title>{postData.title}</title>
-        </Head>
-        <article>
-          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-          <div className={utilStyles.lightText}>
-            <Date dateString={postData.date} />
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </article>
+        <Container>
+          <Header/>
+          <Head>
+            <title>{postData.title}</title>
+          </Head>
+          <article>
+            <PostHeader
+              title={postData.title}
+              date={postData.date}
+            />
+            {/* <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+              <Date dateString={postData.date} />
+            </div> */}
+            <PostBody content={postData.contentHtml}/>
+            {/* <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} /> */}
+            <div className={styles.backToHome}>
+              <Link href="/">← Back to home</Link>
+            </div>
+          </article>
+        </Container>
       </Layout>
     );
   }
