ae72ab3f7d7560cb65c84d6ef913f7af0cc80004
aniketbhatia28 <aniketbhatia28@gmail.com>
2023-11-18 11:12:21 +0530

    added graphcms to manage blog content

---
+++ pages/posts/[slug].js
@@ -0,0 +1,67 @@
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import Head from 'next/head';
+import Layout from '../../components/layout';
+import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/graphcms';
+import Container from '../../components/container';
+import Header from '../../components/header';
+import PostHeader from '../../components/post-header';
+import PostBody from '../../components/post-body';
+import MoreStories from '../../components/more-stories';
+import SectionSeparator from '../../components/section-separator';
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostAndMorePosts(params.slug)
+  return {
+    props: {
+      post: postData.post,
+      morePosts: postData.morePosts || [],
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = await getAllPostsWithSlug();
+
+  return {
+    paths: posts.map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: true,
+  };
+}
+
+export default function Post({ post, morePosts }) {
+  const router = useRouter()
+
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+    return (
+      <Layout>
+        <Container>
+          <Header/>
+          {router.isFallback ? (
+            <PostTitle>Loading…</PostTitle>
+          ) : (
+            <>
+              <article>
+              <Head>
+                <title>{post.title}</title>
+              </Head>
+              <PostHeader
+                title={post.title}
+                date={post.createdAt}
+                coverImage={post.featuredImage}
+                author={post.author}
+              />
+              <PostBody content={post.content}/>
+            </article>
+            <SectionSeparator />
+            {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+            </>
+          )}
+        </Container>
+      </Layout>
+    );
+  }
