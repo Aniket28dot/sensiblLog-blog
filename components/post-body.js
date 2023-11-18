61175bfa60bf21330e8f243e864b84f664b716ca
aniketbhatia28 <aniketbhatia28@gmail.com>
2023-11-18 18:41:42 +0530

    justified post-body content

--- components/post-body.js
+++ components/post-body.js
@@ -3,7 +3,7 @@ import postStyles from './post-styles.module.css'
 export default function PostBody({ content }) {
   return (
     <div
-      className={`max-w-2xl mx-auto post ${postStyles.post}`}
+      className={`max-w-2xl mx-auto post ${postStyles.post} text-justify`}
       dangerouslySetInnerHTML={{ __html: content?.html }}
     />
   )
