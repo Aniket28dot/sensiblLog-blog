be273d26ef866a50511e36d91c5f2e3c4ec98078
aniketbhatia28 <aniketbhatia28@gmail.com>
2024-02-16 14:46:44 +0530

    added revalidate param in getStaticProps()

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export async function getStaticProps() {
     props: {
       allPostsData,
     },
+    revalidate: 10
   };
 }
 
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -69,6 +69,7 @@ export async function getStaticProps({ params }) {
       post: postData.post,
       morePosts: postData.morePosts || [],
     },
+    revalidate: 10
   }
 }
 
